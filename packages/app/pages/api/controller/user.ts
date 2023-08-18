a3a190e69a89a2ee6dcb842d993ae0731dcba302
benrobo <alumonabenaiah71@gmail.com>
2023-08-18 04:43:27 +0100

    added custom error

--- packages/app/pages/api/controller/user.ts
+++ packages/app/pages/api/controller/user.ts
@@ -1,5 +1,19 @@
+import ServerResponseError from "../helper/errorHandler";
+import { CreateUserSchema } from "../helper/validator";
+
+interface CreateUserProps {
+  fullname: string;
+  id: string;
+  email: string;
+  role: "MERCHANT" | "SUPPLIER" | "BUYER";
+}
+
 export default class UserController {
   constructor() {}
+
+  private isValidationError(error: any) {
+    return typeof error !== "undefined";
+  }
 
   async getUser(id: string) {
     throw Error("User not found");
@@ -7,5 +21,12 @@ export default class UserController {
 
   async getUsers() {
     throw Error("User not found");
+  }
+
+  async createUser(payload: CreateUserProps) {
+    const { error, value } = CreateUserSchema.validate(payload);
+    if (this.isValidationError(error)) {
+      throw new ServerResponseError("CODE", "message");
+    }
   }
 }

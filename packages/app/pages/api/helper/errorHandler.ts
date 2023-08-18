fa5d1f2dc78674747ee3e90b6087792acc22e36e
benrobo <alumonabenaiah71@gmail.com>
2023-08-18 04:43:32 +0100

    added custom error

---
+++ packages/app/pages/api/helper/errorHandler.ts
@@ -0,0 +1,15 @@
+import { GraphQLError } from "graphql";
+
+class ServerResponseError extends Error {
+  extensions;
+  isCustomError;
+  constructor(code: string, message: string) {
+    super(message);
+    this.name = "ServerResponseError";
+    this.extensions = { code };
+
+    throw new GraphQLError(message, { extensions: { code: code } });
+  }
+}
+
+export default ServerResponseError;

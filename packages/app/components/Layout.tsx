26bf4444b9c5058b57f1a4e2ef6f5dc43ad099a7
benrobo <alumonabenaiah71@gmail.com>
2023-08-19 04:22:53 +0100

    added major changes

--- packages/app/components/Layout.tsx
+++ packages/app/components/Layout.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Pattern from "./Pattern";
 import { twMerge } from "tailwind-merge";
+import BottomNav from "./BottomNav";
 
 interface LayoutProps {
   children?: React.ReactNode;
   className?: React.ComponentProps<"div">["className"];
+  activePage?: string;
 }
 
 function Layout({ children, className }: LayoutProps) {
@@ -22,10 +24,11 @@ function Layout({ children, className }: LayoutProps) {
 
 export default Layout;
 
-export function MobileLayout({ children }: LayoutProps) {
+export function MobileLayout({ children, activePage }: LayoutProps) {
   return (
     <div className="sm:w-full md:max-w-[500px] xl:max-w-[500px] mx-auto relative h-[100vh] overflow-y-auto">
       <div className="z-upper">{children}</div>
+      <BottomNav activePage={activePage as string} />
     </div>
   );
 }

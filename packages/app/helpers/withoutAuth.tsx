70a39f79646db35acd226ad70e44de7faaf5bacf
benrobo <alumonabenaiah71@gmail.com>
2023-08-19 01:36:32 +0100

    added major changes

---
+++ packages/app/helpers/withoutAuth.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { clerkClient, getAuth, buildClerkProps } from "@clerk/nextjs/server";
+import { GetServerSideProps } from "next";
+import { useRouter } from "next/router";
+import { useAuth } from "@clerk/nextjs";
+import useIsAuth from "./useIsAuth";
+
+interface WithAuthProps {
+  fn: () => React.ReactElement;
+  clerk?: any;
+}
+
+// mostly used for auth page
+const withoutAuth = <P extends {}>(
+  WrappedComponent: React.ComponentType<P>
+) => {
+  const Wrapper: React.FC<P> = (props) => {
+    const router = useRouter();
+    const isLoggedIn = useIsAuth();
+
+    React.useEffect(() => {
+      if (isLoggedIn) {
+        router.push("/dashboard");
+      }
+    }, []);
+
+    return <WrappedComponent {...props} />;
+  };
+
+  return Wrapper;
+};
+
+export default withoutAuth;

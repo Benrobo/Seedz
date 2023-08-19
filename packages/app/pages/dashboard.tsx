51161f8ec30c46d4450e7a78115db8ac3e743a84
benrobo <alumonabenaiah71@gmail.com>
2023-08-19 04:22:46 +0100

    added major changes

--- packages/app/pages/dashboard.tsx
+++ packages/app/pages/dashboard.tsx
@@ -1,20 +1,22 @@
 import BlurBgRadial from "@/components/BlurBgRadial";
 import BottomNav from "@/components/BottomNav";
 import Layout, { MobileLayout } from "@/components/Layout";
+import { ChildBlurModal } from "@/components/Modal";
 import withAuth from "@/helpers/withAuth";
 import { UserButton } from "@clerk/nextjs";
 import React from "react";
 import { BiCurrentLocation } from "react-icons/bi";
 import { BsBank2 } from "react-icons/bs";
 import { FaTemperatureHigh } from "react-icons/fa";
-import { IoAddOutline } from "react-icons/io5";
+import { IoAddOutline, IoCashOutline } from "react-icons/io5";
+import { MdDoubleArrow } from "react-icons/md";
 
 function Dashboard() {
   return (
     <Layout className="bg-white-105">
-      <MobileLayout>
+      <MobileLayout activePage="home">
         <div className="w-full h-[100vh] relative bg-white-100 flex flex-col items-center justify-start overflow-x-hidden ">
-          <div className="w-full h-[450px] relative overflow-hidden bg-dark-100 before:content-[''] before:absolute before:top-[-10em] before:left-[5em] before:w-[60%] before:h-[100vh] before:bg-dark-200 before:rotate-[120deg] flex items-start justify-start ">
+          <div className="w-full h-[650px] relative overflow-hidden bg-dark-100 before:content-[''] before:absolute before:top-[-10em] before:left-[5em] before:w-[60%] before:h-[100vh] before:bg-dark-200 before:rotate-[120deg] flex items-start justify-start ">
             <div className="w-full z-[1] flex items-start justify-between px-[2em] px-md:[3em] mt-[4em] ">
               <div className="w-full flex flex-col items-start justify-start">
                 <h1 className="N-EB text-white-200 text-3xl max-w-[250px] ">
@@ -31,7 +33,7 @@ function Dashboard() {
             </div>
 
             {/* mini weather info */}
-            <div className="w-full px-3 py-2 absolute bottom-[2.4em] right-0 flex items-center justify-end gap-5">
+            <div className="w-full px-3 py-2 absolute bottom-[4.4em] right-0 flex items-center justify-end gap-5">
               <div className="flex items-center justify-start gap-1">
                 <FaTemperatureHigh className="text-green-600" />
                 <span className="N-B text-[14px] text-white-100">20</span>
@@ -46,7 +48,7 @@ function Dashboard() {
           </div>
 
           {/* Wallet Section */}
-          <div className="w-full relative z-[100] top-[-1.5em]  flex flex-col items-center justify-center px-[3.5em]  ">
+          <div className="w-full relative z-[100] top-[-3.5em]  flex flex-col items-center justify-center px-[3.5em]  ">
             {/* style needed to add a mini box uder the div */}
             {/* before:content-[''] before:w-[70%] before:h-[100px] before:z-[-5] before:absolute before:bottom-[-.7em] before:mx-auto before:bg-green-300 before:shadow-sm before:rounded-[20px] */}
             <div className="w-full z-[2] relative flex flex-col items-center justify-center bg-white-100 shadow-lg rounded-[10px] py-5 ">
@@ -67,9 +69,43 @@ function Dashboard() {
             </div>
           </div>
 
-          <main className="w-full h-full z-[100] max-h-full hideScrollBar overflow-y-scroll "></main>
-          <BottomNav />
+          <main className="w-full h-full z-[100] max-h-full hideScrollBar overflow-y-scroll ">
+            {/* some other component here */}
+          </main>
         </div>
+
+        {/* Top up modal */}
+        <ChildBlurModal
+          isBlurBg
+          isOpen={true}
+          showCloseIcon
+          className="bg-white-600"
+        >
+          <div className="w-full flex flex-col items-center justify-center">
+            <div className="w-full px-[4em] flex flex-col items-center justify-center ">
+              <p className="text-white-400 ppR text-[12px] mb-2">Amount</p>
+              <h1 className="text-dark-100 N-EB text-5xl">$100</h1>
+            </div>
+            <br />
+            <div className="px-10 max-w-[400px] flex flex-col items-center justify-center">
+              <div className="w-full bg-dark-200 border-[2px] border-solid border-dark-100 flex items-start justify-start rounded-r-[5px] rounded-t-[5px]">
+                <input
+                  type="number"
+                  className="w-full text-center h-[45px] border-none outline-none rounded-none text-dark-100 N-B text-2xl px-4 py-3 "
+                />
+                <button className="w-[70px] h-[45px] flex flex-col items-center justify-center text-white-100 bg-dark-300 N-B rounded-r-[10px] rounded-t-[10px] ">
+                  <IoCashOutline size={20} />
+                </button>
+              </div>
+
+              <br />
+              <button className="w-full px-6 py-4 rounded-[5px] flex items-center justify-center bg-green-700 text-white-100 ppR">
+                <span className="text-[14px] N-B ">Continue</span>{" "}
+                <MdDoubleArrow className="ml-2 text-white-100" />
+              </button>
+            </div>
+          </div>
+        </ChildBlurModal>
       </MobileLayout>
     </Layout>
   );

c8d1a08c04114d792843c273a5b125bc784399cb
benrobo <alumonabenaiah71@gmail.com>
2023-08-19 01:36:29 +0100

    added major changes

--- packages/app/helpers/withAuth.tsx
+++ packages/app/helpers/withAuth.tsx
@@ -3,24 +3,17 @@ import { clerkClient, getAuth, buildClerkProps } from "@clerk/nextjs/server";
 import { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
 import { useAuth } from "@clerk/nextjs";
+import useIsAuth from "./useIsAuth";
 
-interface WithAuthProps {
-  fn: () => React.ReactElement;
-  clerk?: any;
-}
-
+// mostly used for other pages aside from auth page
 const withAuth = <P extends {}>(WrappedComponent: React.ComponentType<P>) => {
   const Wrapper: React.FC<P> = (props) => {
     const router = useRouter();
-    const { isLoaded, userId, sessionId, getToken } = useAuth();
-
-    console.log({ log: userId, sessionId, getToken: getToken() });
+    const isLoggedIn = useIsAuth();
 
     React.useEffect(() => {
-      const isLoggedIn = true;
-      // const isLoggedIn = checkIfUserIsLoggedIn();
       if (!isLoggedIn) {
-        router.push("/login");
+        router.push("/auth/login");
       }
     }, []);
 

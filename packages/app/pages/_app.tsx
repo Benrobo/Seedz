c5e19602aa02c957599e3156dfdebfda0a4b7614
benrobo <alumonabenaiah71@gmail.com>
2023-08-19 01:36:34 +0100

    added major changes

--- packages/app/pages/_app.tsx
+++ packages/app/pages/_app.tsx
@@ -1,13 +1,35 @@
 import "@/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "react-query";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  from,
+} from "@apollo/client";
+import ENV from "./api/config/env";
+import { errorLink, httpLink } from "@/helpers/clientError";
+import { Toaster } from "react-hot-toast";
 
 const publicPages = ["/auth/login", "/auth/signup"];
+const queryClient = new QueryClient();
+
+const client = new ApolloClient({
+  uri: ENV.serverUrl,
+  cache: new InMemoryCache(),
+  link: from([errorLink, httpLink]),
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />
-    </ClerkProvider>
+    <ApolloProvider client={client}>
+      <QueryClientProvider client={queryClient}>
+        <ClerkProvider {...pageProps}>
+          <Component {...pageProps} />
+          <Toaster />
+        </ClerkProvider>
+      </QueryClientProvider>
+    </ApolloProvider>
   );
 }

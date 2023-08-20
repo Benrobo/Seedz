cac7a597ec3cd34b75f98c9c2e62428609244e5e
benrobo <alumonabenaiah71@gmail.com>
2023-08-20 20:23:23 +0100

    added major update

--- packages/app/pages/api/helper/validator.ts
+++ packages/app/pages/api/helper/validator.ts
@@ -9,3 +9,13 @@ export const InitPaymentSchema = Joi.object({
   amount: Joi.string().required(),
   currency: Joi.string().required(),
 });
+
+export const AddProductSchema = Joi.object({
+  name: Joi.string().required(),
+  category: Joi.string().required(),
+  price: Joi.string().required(),
+  availableForRent: Joi.boolean().required(),
+  rentingPrice: Joi.string().required(),
+  base64Image: Joi.string().required(),
+  description: Joi.string().required(),
+});

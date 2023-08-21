a3b91d0cf11cea61c8fe9337b03e9e6cdf3491fd
benrobo <alumonabenaiah71@gmail.com>
2023-08-21 21:40:16 +0100

    started product checkout and payment

--- packages/app/pages/api/controller/product.ts
+++ packages/app/pages/api/controller/product.ts
@@ -1,10 +1,19 @@
-import { ApiAddProductProp, FundWalletType } from "../../../@types";
+import {
+  ApiAddProductProp,
+  ApiProductCheckoutProps,
+  FundWalletType,
+} from "../../../@types";
 import prisma from "../config/prisma";
 import ServerResponseError from "../helper/errorHandler";
-import { AddProductSchema, InitPaymentSchema } from "../helper/validator";
+import {
+  AddProductSchema,
+  InitPaymentSchema,
+  ProductCheckoutSchema,
+} from "../helper/validator";
 import { genID } from "../helper";
 import $http from "../config/axios";
 
+type productQtyArray = { prodId: string; qty: number }[];
 export default class ProductController {
   constructor() {}
 
@@ -12,68 +21,40 @@ export default class ProductController {
     return typeof error !== "undefined";
   }
 
-  // init paystack payment
-  async initPsPayment(amount: number, email: string) {
-    let result = { success: false, data: null, msg: null || "" };
-    try {
-      const req = await $http.post("/transaction/initialize", {
-        amount,
-        email,
-      });
-      const res = req?.data;
-      if (res.status) {
-        result["success"] = true;
-        result["data"] = res.data;
-        result["msg"] = res.message;
-        return result;
+  private async validateProductQuantities(productQtyArray: productQtyArray) {
+    const products = await this.checkAvailableQuantities(productQtyArray);
+
+    const validationResults = productQtyArray.map((item) => {
+      const requestedQty = item.qty;
+      const product = products.find((p) => p.id === item.prodId);
+      if (!product) {
+        return {
+          prodId: item.prodId,
+          isValid: false,
+          reason: "Product not found",
+        };
       }
-      result["success"] = false;
-      result["data"] = null;
-      result["msg"] = "Something went wrong initializing payment.";
-      return result;
-    } catch (e: any) {
-      console.log(e.response.data.message ?? e.message);
-      console.log(
-        `Error initializing payment: ${e.response.data.message ?? e.message}`
-      );
-      result["success"] = false;
-      result["data"] = null;
-      result["msg"] = `Error initializing payment: ${
-        e.response.data.message ?? e.message
-      }`;
-      return result;
-    }
+      const availableQty = product.quantity;
+      const isValid = requestedQty <= availableQty;
+      return {
+        prodId: item.prodId,
+        isValid,
+        reason: isValid ? "Available" : "Insufficient quantity",
+      };
+    });
+
+    return validationResults;
   }
 
-  async fundWallet(payload: FundWalletType, userId: string) {
-    const { error, value } = InitPaymentSchema.validate(payload);
-    if (this.isValidationError(error)) {
-      throw new ServerResponseError("INVALID_FIELDS", (error as any).message);
-    }
+  private async checkAvailableQuantities(productQtyArray: productQtyArray) {
+    const productIds = productQtyArray.map((item) => item.prodId);
+    const products = await prisma.products.findMany({
+      where: {
+        id: { in: productIds },
+      },
+    });
 
-    const { amount, currency } = payload;
-    const validCurr = "NGN";
-
-    if (currency !== validCurr) {
-      throw new ServerResponseError("INVALID_CURRENCY", "currency is invalid.");
-    }
-
-    // init payment
-    const user = await prisma.users.findFirst({ where: { id: userId } });
-    const result = await this.initPsPayment(
-      Number(amount * 100),
-      user?.email as string
-    );
-
-    if (result.success === true) {
-      return {
-        authorization_url: (result?.data as any).authorization_url,
-        access_code: (result?.data as any).access_code,
-        reference: (result?.data as any).reference,
-      };
-    }
-
-    throw new ServerResponseError("INIT_PAYMENT_ERROR", result.msg);
+    return products;
   }
 
   async addProduct(payload: ApiAddProductProp, userId: string) {
@@ -121,5 +102,16 @@ export default class ProductController {
     return await prisma.products.findMany({
       include: { image: true, ratings: true, user: true },
     });
+  }
+
+  async productCheckout(payload: ApiProductCheckoutProps, userId: string) {
+    const { error, value } = ProductCheckoutSchema.validate(payload);
+    if (this.isValidationError(error)) {
+      throw new ServerResponseError("INVALID_FIELDS", (error as any).message);
+    }
+
+    const { totalAmount, productQty } = payload;
+
+    console.log(totalAmount, productQty);
   }
 }

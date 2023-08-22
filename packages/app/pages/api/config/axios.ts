2ffe1fb4f6645443b4490645112adcfb36d4d928
benrobo <alumonabenaiah71@gmail.com>
2023-08-22 01:22:25 +0100

    completed productCheckout

--- packages/app/pages/api/config/axios.ts
+++ packages/app/pages/api/config/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const $http = axios.create({
   baseURL: "https://api.paystack.co",
-  timeout: 1000,
+  timeout: 5000,
   headers: {
     Authorization: `Bearer ${process.env.PS_TEST_SEC}`,
   },

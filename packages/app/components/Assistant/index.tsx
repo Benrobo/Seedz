44818ef3cd2e2b44ffbb7c770c22cc0924ef5013
benrobo <alumonabenaiah71@gmail.com>
2023-08-23 03:03:28 +0100

    added major update to ai assistant

--- packages/app/components/Assistant/index.tsx
+++ packages/app/components/Assistant/index.tsx
@@ -7,6 +7,7 @@ import { IoIosArrowBack } from "react-icons/io";
 import { useMutation } from "@apollo/client";
 import { SeedzAssistant } from "@/pages/http";
 import handleApolloHttpErrors from "@/pages/http/error";
+import MarkdownRenderer from "../MarkdownRender";
 
 const chatLanguages = [
   {
@@ -26,8 +27,8 @@ interface AssistantProps {
 }
 
 interface ChatMessage {
-  message: string;
-  type: "bot" | "user";
+  message: string[] | string;
+  type: "bot" | "user" | string;
   lang: string;
   isError: boolean;
 }
@@ -38,30 +39,19 @@ function Assistant({
   isOpen,
 }: AssistantProps) {
   const [settingsModal, setSettingsModal] = React.useState(false);
-  const [selectedLang, setSelectedLang] = React.useState("");
+  const [selectedLang, setSelectedLang] = React.useState("en");
   const [welcomePage, setWelcomePage] = React.useState(false);
   const [messages, setMessages] = React.useState([] as ChatMessage[]);
-  const [askSeedzAiMut, { loading, error, reset, data }] =
-    useMutation(SeedzAssistant);
+  const [askSeedzAiMut, { loading, error, reset, data }] = useMutation(
+    SeedzAssistant,
+    { errorPolicy: "none" }
+  );
   const [userMsg, setUserMsg] = React.useState("");
+  const [speech, setSpeech] = React.useState("");
 
-  // save messages in local storage
-  React.useEffect(() => {
-    if (typeof window !== "undefined") {
-      setInterval(() => {
-        if (messages.length > 0) {
-          localStorage.setItem("@seedz_ai_resp", JSON.stringify(messages));
-        }
-      }, 1000);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const messagesEndRef = React.useRef(null);
 
   React.useEffect(() => {
-    if (messages !== null) {
-      setMessages(messages);
-    }
-
     if (typeof window !== "undefined") {
       const welcomePage =
         localStorage.getItem("@ai_welcome_page") === null
@@ -75,7 +65,7 @@ function Assistant({
         localStorage.getItem("@seedz_ai_resp") === null
           ? null
           : JSON.parse(localStorage.getItem("@seedz_ai_resp") as string);
-      setMessages(messages);
+      setMessages(messages ?? []);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
@@ -84,46 +74,100 @@ function Assistant({
     if (messages.length > 0) {
       setWelcomePage(false);
       localStorage.setItem("@ai_welcome_page", JSON.stringify(false));
+      localStorage.setItem("@seedz_ai_resp", JSON.stringify(messages));
     }
   }, [messages]);
 
   React.useEffect(() => {
     reset();
     if (error) {
-      const err = handleApolloHttpErrors(error);
-      setMessages((prev) => [
-        ...prev,
+      const networkError = error.networkError as any;
+      const errorObj =
+        networkError?.result?.errors[0] ?? error.graphQLErrors[0];
+      const err = errorObj?.message;
+      const combMsg = [
+        ...messages,
         {
-          isError: true,
-          lang: "en",
-          message: err,
+          isError: false,
+          lang: data?.askSeedzAi?.answer?.lang,
+          message: err ?? null,
           type: "bot",
         },
-      ]);
+      ];
+      setMessages(combMsg);
+      localStorage.setItem("@seedz_ai_resp", JSON.stringify(combMsg));
+      scrollToBottom();
     } else if (typeof data?.askSeedzAi.success !== "undefined") {
-      console.log(data);
+      const combMsg = [
+        ...messages,
+        {
+          isError: false,
+          lang: data?.askSeedzAi?.answer?.lang,
+          message: data?.askSeedzAi?.answer?.join("") ?? null,
+          type: "bot",
+        },
+      ];
+      setMessages(combMsg);
+      localStorage.setItem("@seedz_ai_resp", JSON.stringify(combMsg));
+      scrollToBottom();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [data, error]);
 
+  React.useEffect(() => {
+    scrollToBottom();
+  }, []);
+
+  function sendInitialAiMsg(msg: string) {
+    if (msg.length === 0) return;
+
+    const payload = {
+      question: msg,
+      lang: selectedLang ?? "en",
+    };
+
+    askSeedzAiMut({ variables: { AiInput: payload } });
+  }
+
   function sendMessage() {
     if (userMsg.length === 0) return;
+
     const payload = {
       question: userMsg,
       lang: selectedLang ?? "en",
     };
-    askSeedzAiMut({
-      variables: { AiInput: payload },
-    });
+
+    const combMsg = [
+      ...messages,
+      {
+        isError: false,
+        lang: selectedLang ?? "en",
+        message: userMsg,
+        type: "user",
+      },
+    ];
+
+    askSeedzAiMut({ variables: { AiInput: payload } });
+    setMessages(combMsg);
+    setUserMsg("");
+    localStorage.setItem("@seedz_ai_resp", JSON.stringify(combMsg));
+    scrollToBottom();
   }
+
+  // scroll to bottom implementation
+  const scrollToBottom = () => {
+    (messagesEndRef as any).current?.scrollIntoView({
+      behavior: "smooth",
+    });
+  };
 
   return (
     <>
       <button
-        className="w-[50px] h-[50px] p-3 flex flex-col items-center justify-center bg-green-600 text-white-100 rounded-[50%] absolute bottom-[7em] right-[2em] shadow-lg scale-[.95] hover:scale-[1] transition-all cursor-pointer z-[100] "
+        className="w-[50px] h-[50px] flex flex-col items-center justify-center bg-green-600 text-white-100 rounded-[50%] absolute bottom-[7em] right-[2em] shadow-lg transition-all cursor-pointer z-[100] hover:animate-pulse "
         onClick={openAssistant}
       >
-        <BiSolidBot size={20} />
+        <BiSolidBot size={45} className="p-3" />
       </button>
       <ChildBlurModal isOpen={isOpen} className="bg-white-105 hideScrollBar">
         <div className="w-full h-[100vh] flex flex-col items-center justify-center overflow-hidden">
@@ -146,54 +190,75 @@ function Assistant({
               </button>
             </div>
           )}
-          {welcomePage && <WelcomeScreen setMessage={setMessages} />}
+          {messages?.length === 0 && (
+            <WelcomeScreen
+              sendInitialAiMsg={sendInitialAiMsg}
+              setMessage={setMessages}
+            />
+          )}
           {/* Main chat area */}
-          {welcomePage === false && (
+          {messages?.length > 0 && (
             <div className="w-full h-[100vh] flex flex-col items-start justify-start overflow-y-auto hideScrollBar px-4 gap-5">
               {/* Gap */}
               <div className="w-full min-h-[60px] "></div>
 
-              {messages.map((m) => (
-                <div
-                  key={m.message.length * Math.random() * 100}
-                  className="w-full flex flex-col items-start justify-start gap-3"
-                >
-                  {m.type === "user" && (
-                    <div className="w-full flex flex-col items-end justify-end">
-                      <div className="w-auto max-w-[300px] h-auto bg-white2-400 text-dark-100 flex flex-col items-start justify-start px-3 py-3 text-[12px] ppR rounded-md ">
-                        {m.message}
-                      </div>
-                    </div>
-                  )}
-                  {m.type === "bot" && (
+              {messages?.length > 0
+                ? messages?.map((m) => (
                     <div
-                      key={m.message.length * Math.random() * 100}
-                      className="w-full flex flex-col items-start justify-start"
+                      key={m?.message?.length * Math.random() * 100}
+                      className="w-full flex flex-col items-start justify-start gap-3"
                     >
-                      <div className="w-full max-w-[300px] h-auto bg-green-600 text-white-100 flex flex-col items-start justify-start px-3 py-3 text-[12px] ppR rounded-md ">
-                        {m.message}
-                      </div>
-                    </div>
-                  )}
-                  {loading && (
-                    <div
-                      key={m.message.length * Math.random() * 100}
-                      className="w-full flex flex-col items-start justify-start"
-                    >
-                      <div className="w-auto max-w-[100px] h-auto bg-green-600 text-white-100 flex flex-col items-start justify-start px-3 py-3 text-[12px] ppR rounded-md chatLoading ">
-                        <div className="w-full lds-ellipsis">
-                          <div></div>
-                          <div></div>
-                          <div></div>
-                          <div></div>
+                      {m.type === "user" && (
+                        <div className="w-full flex flex-col items-end justify-end">
+                          <div className="w-auto max-w-[300px] h-auto bg-dark-400 text-white-100 flex flex-col items-start justify-start px-3 py-3 text-[12px] ppR rounded-md ">
+                            {m.message}
+                          </div>
                         </div>
-                      </div>
+                      )}
+                      {m.type === "bot" && (
+                        <div
+                          key={m?.message?.length * Math.random() * 100}
+                          className="w-full flex flex-col items-start justify-start"
+                        >
+                          <div
+                            className={`w-auto bg-white2-300 h-auto ${
+                              m.isError
+                                ? "border-solid border-[2px] border-red-305 "
+                                : ""
+                            } text-dark-100 whitespace-pre-wrap flex flex-col items-start justify-start px-3 py-3 text-[12px] ppR rounded-md ChatMsg`}
+                          >
+                            {
+                              <MarkdownRenderer
+                                content={
+                                  Array.isArray(m.message)
+                                    ? m.message.join("")
+                                    : m.message
+                                }
+                              />
+                            }
+                          </div>
+                        </div>
+                      )}
                     </div>
-                  )}
-                </div>
-              ))}
+                  ))
+                : null}
 
-              <div className="w-full min-h-[90px] "></div>
+              <div ref={messagesEndRef}></div>
+
+              {loading && (
+                <div className="w-full flex flex-col items-start justify-start">
+                  <div className="w-auto max-w-[100px] h-auto bg-white2-300 text-white-100 flex flex-col items-start justify-start px-3 py-3 text-[12px] ppR rounded-md chatLoading ">
+                    <div className="w-full lds-ellipsis">
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                      <div></div>
+                    </div>
+                  </div>
+                </div>
+              )}
+
+              <div className="w-full min-h-[120px] "></div>
             </div>
           )}
 
@@ -219,7 +284,7 @@ function Assistant({
                     name="language"
                     id=""
                     className="w-full mt-2 text-[14px] text-dark-400 bg-white2-300 px-3 py-2 rounded-md ppR"
-                    //   onChange={handleControlInp}
+                    onChange={(e) => setSelectedLang(e.target.value)}
                   >
                     <option value="">select language</option>
                     {chatLanguages.map((d) => (
@@ -230,11 +295,11 @@ function Assistant({
                   </select>
                 </div>
                 <br />
-                <div className="w-full flex flex-col items-center justify-center px-3">
+                {/* <div className="w-full flex flex-col items-center justify-center px-3">
                   <button className="w-full bg-green-600 text-white-100 rounded-[30px] ppM text-[14px] flex items-center justify-center text-center px-3 py-3">
                     Save Settings
                   </button>
-                </div>
+                </div> */}
                 <br />
               </div>
             </div>
@@ -243,19 +308,24 @@ function Assistant({
           {/* chat section */}
           <div className="w-full absolute bottom-0 right-0 flex flex-col items-center justify-center backdrop-blur bg-white-500 bg-opacity-75 px-3 py-3">
             <div className="w-full flex items-center justify-center p-1 rounded-md bg-dark-300 ">
-              <textarea
+              <input
                 name=""
-                rows={2}
+                type="text"
                 className="w-full max-h-[50px] text-white-200 text-[14px] ppR py-2 resize-none outline-none px-2 border-none bg-transparent text-white"
                 placeholder="Message here..."
+                // defaultValue={userMsg}
+                value={userMsg}
                 onChange={(e) => setUserMsg(e.target.value)}
                 onKeyDown={(e) => {
-                  if (e.key === "enter") {
+                  if (e.key === "Enter") {
                     sendMessage();
                   }
                 }}
-              ></textarea>
-              <button className="w-[60px] h-[50px] bg-green-600 text-white-100 rounded-md  ppM flex flex-col text-center items-center justify-center">
+              />
+              <button
+                className="w-[60px] h-[50px] bg-green-600 text-white-100 rounded-md  ppM flex flex-col text-center items-center justify-center scale-[.85]"
+                onClick={sendMessage}
+              >
                 <BiSend size={20} />
               </button>
             </div>
@@ -270,9 +340,10 @@ export default Assistant;
 
 interface WelcomeScreenProps {
   setMessage: React.Dispatch<React.SetStateAction<ChatMessage[]>>;
+  sendInitialAiMsg: (val: string) => void;
 }
 
-function WelcomeScreen({ setMessage }: WelcomeScreenProps) {
+function WelcomeScreen({ setMessage, sendInitialAiMsg }: WelcomeScreenProps) {
   const exampleMessages = ["How to improve soil condition"];
 
   return (
@@ -297,12 +368,13 @@ function WelcomeScreen({ setMessage }: WelcomeScreenProps) {
           <button
             key={d}
             className="w-full max-w-[140px] bg-white-300 rounded-md ppM text-[12px] flex text-center px-3 py-3"
-            onClick={() =>
+            onClick={() => {
               setMessage((prev) => [
                 ...prev,
                 { message: d, lang: "en", type: "user", isError: false },
-              ])
-            }
+              ]);
+              sendInitialAiMsg(d);
+            }}
           >
             How to improve soil condition
           </button>
